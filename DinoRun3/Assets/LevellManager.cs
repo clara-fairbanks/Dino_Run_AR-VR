6bcf5f425bcc7d8a4d50e32c897668c056673098
clarafairbanks <clarafairbanks@gmail.com>
2017-05-04 18:22:10 -0400

    DinoRunV2
    added ar
    created infinite path for jungle scene
    added point system
    updated ui

---
+++ DinoRun3/Assets/LevellManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevellManager : MonoBehaviour {
+
+	public void LoadScene(string name){
+		Application.LoadLevel(name);
+	}
+}
