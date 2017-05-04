9d09738ee3495a478559cd0d3eb0f176d78089a7
clarafairbanks <clarafairbanks@gmail.com>
2017-05-04 01:21:48 -0400

    JungleScene finished
    Added Point system
    created infinite jungle path
    updated ui
    **switched from ac controller to default controller for testing
    (comp_cs. is test controller,
    comp_AC is accerlometer)
    
    libraries used:
    infinite runner3d
    jp comp
    3d jungle pack

---
+++ DinoRun/Assets/Jungle/Collide.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collide : MonoBehaviour {
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Explosive")
+			Destroy (gameObject);
+	}
+}
